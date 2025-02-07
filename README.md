9642be757cb5e964088d342aeefb3628874551ae
rrbotlab at debian <4213391+rrbotlab@users.noreply.github.com>
2025-02-07 15:19:48 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,37 @@
+# Challenge Amigo Secreto
+
+![GitHub license](https://badgen.net/github/license/rrbotlab/challenge-amigo-secreto)
+![GitHub license](https://badgen.net/github/commits/rrbotlab/challenge-amigo-secreto)
+![GitHub license](https://badgen.net/github/last-commit/rrbotlab/challenge-amigo-secreto)
+
+
+Cadastre amigos em uma lista e então sorteie um entre eles!
+
+Esse projeto é um desafio exigido na ONE - Oracle Next Education e Alura Latam.
+
+
+## 💻 Pré-requisitos
+
+Navegador atualizado com JavaScript ativado. 
+
+
+## ☕ Uso
+
+- Adicione individualmente os nomes dos amigos;
+- A lista com todos os amigos será atualizada e exibida;
+- Quando quiser, pressione o botão Sortear Amigo;
+- O amigo sorteado será exibido;
+- A lista será eliminada;
+
+
+## ✅ Validações
+
+- Não insere nomes vazios ou duplicados;
+- Não realiza sorteio com menos de 2 amigos na lista;
+
+
+## 🧪 Lab
+
+Acesse [aqui](https://rrbotlab.github.io/challenge-amigo-secreto/) a versão de demonstração do projeto.
+
+
--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// 2024 Ricardo G
+
+let amigos = [];
+
+function adicionarAmigo(){
+
+    // limpa resultado anterior antes de adicionar
+    document.getElementById('resultado').innerHTML = '';
+    
+    let input = document.getElementById('amigo');
+    let nome = input.value.trim().toUpperCase();
+
+    if(!nome){
+        alert('Por favor insira um nome!');
+    }else if(!amigos.includes(nome)){
+        amigos.push(nome);
+        input.focus();
+    }else{
+        alert('"' + nome + '" já existe na lista!');
+    }
+    
+    input.value = '';
+    atualizarListaAmigos();
+
+}
+
+function atualizarListaAmigos(){
+    // console.log(amigos);
+
+    let listaAtualizada = '';
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        listaAtualizada += '<li>' + amigos[i] + '</li>';
+    }
+    
+    lista.innerHTML = listaAtualizada;
+    
+}
+
+function sortearAmigo(){
+
+    // se a lista < 2 retorna
+    if(amigos.length < 2){
+        // console.log('empty');
+        alert('A lista deve ter pelo menos 2 amigos para o sorteio!');
+        return;
+    } 
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    // console.log(indiceAleatorio);
+
+    // limpa lista, exibe sorteado, limpa array amigos 
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('resultado').innerHTML = '<li>O amigo secreto sorteado é: ' + amigos[indiceAleatorio] + '</li>';
+    amigos = [];
+    
+}
